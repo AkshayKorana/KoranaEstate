9e0b4df2f5b796c63821ca135e8f32ac93c29975
201910100695 <92665403+201910100695@users.noreply.github.com>
2026-04-21 22:30:58 +0530

    fix: add NotificationService to JobsModule providers

--- apps/backend/src/jobs/jobs.module.ts
+++ apps/backend/src/jobs/jobs.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common'
+import { NotificationService } from '../notifications/notification.service'
 import { PricesModule } from '../prices/prices.module'
 import { JobsController } from './jobs.controller'
 import { JobsService } from './jobs.service'
@@ -6,6 +7,6 @@ import { JobsService } from './jobs.service'
 @Module({
   imports: [PricesModule],
   controllers: [JobsController],
-  providers: [JobsService],
+  providers: [JobsService, NotificationService],
 })
 export class JobsModule {}
